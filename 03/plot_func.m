b299e4ab510d8d0d409075041e78d4b14b29ea86
mmheydari97 <heidarymm@yahoo.com>
2019-09-04 01:37:01 +0430

    ch03

---
+++ 03/plot_func.m
@@ -0,0 +1,14 @@
+x=linspace(-2,2,40);y=linspace(-3,3,40);
+[X,Y] = meshgrid(x,y);
+% Z=2+X-Y+2*X.^2+2*X.*Y+Y.^2;
+Z = 10*(Y-X.^2).^2+(X-1).^2;
+subplot(1,2,1);
+cs=contour(X,Y,Z);clabel(cs);
+xlabel('x_1');ylabel('x_2');
+title('(a) Contour plot');grid;
+subplot(1,2,2);
+cs=surfc(X,Y,Z);
+zmin=floor(min(Z));
+zmax=ceil(max(Z));
+xlabel('x_1');ylabel('x_2');zlabel('f(x_1,x_2)');
+title('(b) Mesh plot');
